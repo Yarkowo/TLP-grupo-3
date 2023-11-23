f1a21f043860247092699fbdc9abb8a5e39cffd9
Yarkowo <yarko.bahamonde@usm.cl>
2023-11-23 02:20:34 -0300

    wip filtro

--- certamen3/nuestrapp/views.py
+++ certamen3/nuestrapp/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'nuestrapp/base.html')
+
+    segmentos = ["Comunidad USM","Estudiante","Profesor","Jefe de Carrera"]
+    tipos = ["Vacaciones","Feriado","Suspensión de actividades","Suspensión de actividades PM","Periodo Lectivo","Suspensión de evaluaciones","Ceremonia","EDDA","Evaluación","Ayudantías","Hito Académico","Secretaría Académica","OAI",]
+    data={
+        "Segmentos":segmentos,
+        "Tipos":tipos
+    }
+    return render(request, 'nuestrapp/base.html',data)
