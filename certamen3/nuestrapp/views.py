9cff354fb153e9e9e616060fb0161793bf39f206
Yarkowo <yarko.bahamonde@usm.cl>
2023-11-27 01:00:33 -0300

    fixing filtro (problemas con models-views)

--- certamen3/nuestrapp/views.py
+++ certamen3/nuestrapp/views.py
@@ -11,8 +11,16 @@ def index(request):
     segmentos = ["Comunidad_USM","Estudiante","Profesor","Jefe_de_Carrera"]
     tipos = ["Vacaciones","Feriado","Suspensión_de_actividades","Suspensión_de_actividades_PM","Periodo_Lectivo","Suspensión_de_evaluaciones","Ceremonia","EDDA","Evaluación","Ayudantías","Hito_Académico","Secretaría_Académica","OAI",]
     
-    actividades = Evento.objects.all()
-    
+    #filtro
+    if (respuestaSegmento == 'Segmento' or respuestaSegmento == None) and (respuestaTipo == 'Tipo' or respuestaTipo == None): #Segmento none / Tipo none
+        actividades = Evento.objects.all()
+    elif (respuestaSegmento != 'Segmento') and (respuestaTipo != 'Tipo'): #Segmento [X] / Tipo [X]
+        actividades = Evento.objects.filter(tipo_tipo=respuestaTipo).filter(segmento_segmento=respuestaSegmento)
+    elif (respuestaSegmento != 'Segmento') and (respuestaTipo == 'Tipo' or respuestaTipo == None): #Segmento [X] / Tipo none
+        actividades = Evento.objects.filter(tipo_segmento=respuestaSegmento)
+    elif (respuestaSegmento == 'Segmento' or respuestaSegmento == None) and (respuestaTipo != 'Tipo'): #Segmento none / Tipo [X]
+        actividades = Evento.objects.filter(tipo_tipo=respuestaTipo)
+
     data={
         "actividades":actividades,
         "Segmentos":segmentos,
