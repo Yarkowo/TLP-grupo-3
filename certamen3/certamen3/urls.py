46ee61616230e9f7fec18028bf4f0c145470ebf9
Nmcp1 <141276162+Nmcp1@users.noreply.github.com>
2023-11-27 20:09:19 -0300

    agregue los rest (no funca)

--- certamen3/certamen3/urls.py
+++ certamen3/certamen3/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from nuestrapp import views
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('admin/', admin.site.urls),
+    path('rest/',include('rest.urls')),
 ]
