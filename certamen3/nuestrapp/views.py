b06577e112b3da6471cbde341bb8d9cef2baf57d
Yarkowo <yarko.bahamonde@usm.cl>
2023-11-28 03:05:32 -0300

    a

--- certamen3/nuestrapp/views.py
+++ certamen3/nuestrapp/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.hashers import check_password
 
 # Create your views here.
-
+#a
 def index(request):
 
     respuestaSegmento = request.GET.get('Segmento')
