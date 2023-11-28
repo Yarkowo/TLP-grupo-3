2b622a06a25ce54ece20ecfed845b49a71062da9
Yarkowo <yarko.bahamonde@usm.cl>
2023-11-27 21:00:23 -0300

    working filtro

--- certamen3/nuestrapp/models.py
+++ certamen3/nuestrapp/models.py
@@ -13,15 +13,25 @@ class Evento(models.Model):
     titulo = models.CharField(max_length=50)
     descripcion = models.TextField(max_length=300)
     TIPO_TIPO = (
-    ("V","Vacaciones"),
-    ("F","Feriado"),
-    ("S1","Suspensión de actividades"),
-    ("S2", "Suspensión de actividades PM"),
-    ("P","Periodo Lectivo")
-    #Faltan mas
+    ('V',"Vacaciones"),
+    ('F',"Feriado"),
+    ('SA',"Suspensión_de_actividades"),
+    ('SAPM',"Suspensión_de_actividades_PM"),
+    ('PL',"Periodo_Lectivo"),
+    ('SE',"Suspensión_de_evaluaciones"),
+    ('C',"Ceremonia"),
+    ('EDDA',"EDDA"),
+    ('E',"Evaluación"),
+    ('A',"Ayudantías"),
+    ('HA',"Hito_Académico"),
+    ('SECA',"Secretaría_Académica"),
+    ('OAI',"OAI"),
     )    
     tipo_tipo = models.CharField(max_length=10,choices=TIPO_TIPO,default="F")
     tipo_segmento = models.CharField(max_length=10,choices=TIPO_SEGMENTO,default="C")
+
+    # def __str__(tipo_tipo):
+    #    return f"{self.first_name} {self.last_name}"
 
 class UsuarioSegmento(models.Model):
     usuario = models.OneToOneField(User, on_delete=models.CASCADE)
--- certamen3/nuestrapp/views.py
+++ certamen3/nuestrapp/views.py
@@ -8,18 +8,42 @@ def index(request):
     respuestaSegmento = request.GET.get('Segmento')
     respuestaTipo = request.GET.get('Tipo')
 
-    segmentos = ["Comunidad_USM","Estudiante","Profesor","Jefe_de_Carrera"]
-    tipos = ["Vacaciones","Feriado","Suspensión_de_actividades","Suspensión_de_actividades_PM","Periodo_Lectivo","Suspensión_de_evaluaciones","Ceremonia","EDDA","Evaluación","Ayudantías","Hito_Académico","Secretaría_Académica","OAI",]
+    segmentos={"Comunidad_USM":'C',
+                "Estudiante":'E',
+                "Profesor":'P',
+                "Jefe_de_Carrera":'J',
+            }
+    tipos = {"Vacaciones":'V',
+            "Feriado":'F',
+            "Suspensión_de_actividades":'SA',
+            "Suspensión_de_actividades_PM":'SAPM',
+            "Periodo_Lectivo":'PL',
+            "Suspensión_de_evaluaciones":'SE',
+            "Ceremonia":'C',
+            "EDDA":'EDDA',
+            "Evaluación":'E',
+            "Ayudantías":'A',
+            "Hito_Académico":'HA',
+            "Secretaría_Académica":'SECA',
+            "OAI":'OAI',
+        }
     
+    #en cuashro mami en cuashor
+
     #filtro
     if (respuestaSegmento == 'Segmento' or respuestaSegmento == None) and (respuestaTipo == 'Tipo' or respuestaTipo == None): #Segmento none / Tipo none
         actividades = Evento.objects.all()
+        print("filtro None/None")
     elif (respuestaSegmento != 'Segmento') and (respuestaTipo != 'Tipo'): #Segmento [X] / Tipo [X]
-        actividades = Evento.objects.filter(tipo_tipo=respuestaTipo).filter(segmento_segmento=respuestaSegmento)
+        actividades = Evento.objects.filter(tipo_tipo=tipos[respuestaTipo]).filter(tipo_segmento=segmentos[respuestaSegmento])
+        print("filtro Segmento/Tipo")
     elif (respuestaSegmento != 'Segmento') and (respuestaTipo == 'Tipo' or respuestaTipo == None): #Segmento [X] / Tipo none
-        actividades = Evento.objects.filter(tipo_segmento=respuestaSegmento)
+        actividades = Evento.objects.filter(tipo_segmento=segmentos[respuestaSegmento])
+        print("filtro Segmento/None")
     elif (respuestaSegmento == 'Segmento' or respuestaSegmento == None) and (respuestaTipo != 'Tipo'): #Segmento none / Tipo [X]
-        actividades = Evento.objects.filter(tipo_tipo=respuestaTipo)
+        actividades = Evento.objects.filter(tipo_tipo=tipos[respuestaTipo])
+        print("filtro None/Tipo")
+    #fin_filtro
 
     data={
         "actividades":actividades,
